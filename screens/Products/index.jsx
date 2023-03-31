38e4c99666124519ebca5e1e28f8909a8c9ebcda
DeveloperInside <enesm.hancer@gmail.com>
2023-03-31 23:16:57 +0300

    chore: configure vector icons

--- screens/Products/index.jsx
+++ screens/Products/index.jsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Products = () => {
   return (
     <View>
       <Text>Products</Text>
+      <Icon name="rocket" size={30} color="#900" />
     </View>
   );
 };

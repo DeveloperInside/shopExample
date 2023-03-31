6653a9bf2b6a5cf9d6b889a5d74dbe9607b0c5f2
DeveloperInside <enesm.hancer@gmail.com>
2023-03-31 23:54:30 +0300

    feat: product item component created

--- screens/Products/index.jsx
+++ screens/Products/index.jsx
@@ -1,14 +1,15 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Text } from 'react-native'
+import React from 'react'
+import { ProductItem } from '../../components/componentList'
+import { Layout } from '@ui-kitten/components'
 
 const Products = () => {
   return (
-    <View>
+    <Layout>
       <Text>Products</Text>
-      <Icon name="rocket" size={30} color="#900" />
-    </View>
-  );
-};
+      <ProductItem />
+    </Layout>
+  )
+}
 
-export default Products;
+export default Products

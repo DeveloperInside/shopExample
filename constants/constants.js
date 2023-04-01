6e114cae6ef096c5cab314dedd30bc595192945f
DeveloperInside <enesm.hancer@gmail.com>
2023-04-01 18:42:07 +0300

    chore: redux constants

---
+++ constants/constants.js
@@ -0,0 +1,5 @@
+export const REDUX_IDLE = 'idle';
+export const REDUX_SUCCESS = 'ok';
+export const REDUX_ERROR = 'nok';
+export const REDUX_PENDING = 'pending';
+export const REDUX_FULFILLED = 'fulfilled';

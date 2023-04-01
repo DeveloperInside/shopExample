69aff165552e116fae8dbd17d0e472e8fccd6c08
DeveloperInside <enesm.hancer@gmail.com>
2023-04-01 18:50:40 +0300

    feat: dispatch fetchProducts

--- screens/Products/index.jsx
+++ screens/Products/index.jsx
@@ -1,10 +1,19 @@
 import { Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './styles'
 import { Layout } from '@ui-kitten/components'
 import { ProductItem } from 'components/componentList'
+import { useDispatch } from 'react-redux'
+import { fetchProducts } from 'redux/slices/products/extraActions'
 
 const Products = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
+  
+
   return (
     <Layout style={styles.container}>
       <Text>Products</Text>

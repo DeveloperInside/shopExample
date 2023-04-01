aa67943350f1fa1631690452e0dd8eaa5932c575
DeveloperInside <enesm.hancer@gmail.com>
2023-04-01 18:44:12 +0300

    feat: redux provider added and the store connected

--- App.tsx
+++ App.tsx
@@ -5,12 +5,16 @@ import React from 'react'
 import * as eva from '@eva-design/eva'
 import { ApplicationProvider } from '@ui-kitten/components'
 import Navigation from './navigation/Navigation'
+import { Provider } from 'react-redux'
+import { store } from 'redux/store'
 
 function App(): JSX.Element {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
-      <Navigation />
-    </ApplicationProvider>
+    <Provider store={store}>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <Navigation />
+      </ApplicationProvider>
+    </Provider>
   )
 }
 

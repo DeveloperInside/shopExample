2b987a1ad70315bc9134a0932403bc3d19a34663
DeveloperInside <enesm.hancer@gmail.com>
2023-04-01 16:35:43 +0300

    feat: api configured

---
+++ redux/api/productsAPI.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import { APIPATH } from './apiPaths'
+
+export const productsAPI = {
+  fetch: params =>
+    axios.get(APIPATH.BASE + APIPATH.PRODUCT.fetch + '?' + params),
+}

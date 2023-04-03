fd89f3f8408f310d685b19e508d938020bfede71
DeveloperInside <enesm.hancer@gmail.com>
2023-04-03 23:44:40 +0300

    fix: reactotron mock added

---
+++ __mocks__/reactotron-react-native.js
@@ -0,0 +1,10 @@
+const reactotron = {
+  configure: () => reactotron,
+  useReactNative: () => reactotron,
+  use: () => reactotron,
+  connect: () => reactotron,
+  clear: () => reactotron,
+  createEnhancer: () => reactotron
+}
+
+module.exports = reactotron

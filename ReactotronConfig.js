e81bd07238b9e3215e3b0e9942024218c3b99e51
DeveloperInside <enesm.hancer@gmail.com>
2023-04-04 02:23:19 +0300

    fix: AsyncStorage reactotron config excluded

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,8 +1,7 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import Reactotron from 'reactotron-react-native'
 import { reactotronRedux } from 'reactotron-redux'
 
-const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+const reactotron = Reactotron
   .configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux()) //  <- here i am!
